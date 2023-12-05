e180a9b2f123f746751bdb92e945c45737eafe1b
Martin <longmatys@gmail.com>
2023-12-05 16:22:47 +0100

    Day 05 Part 1

---
+++ day 05.py
@@ -0,0 +1,39 @@
+import os
+def reverse_stacks(stacks_reversed):
+    stacks = [[] for _ in range(len(stacks_reversed))]
+    for i in range(len(stacks_reversed)):
+        while len(stacks_reversed[i]):
+            stacks[i].append(stacks_reversed[i].pop())
+    return stacks
+def move_crates(crates,count,start,end):
+    for _ in range(count):
+        a = crates[start-1].pop()
+        crates[end-1].append(a)
+def main():
+# Get the name of the Python script
+    script_name = os.path.basename(__file__)
+    input_file = script_name.split('.')[0]+'.input.txt'
+    stacks_reversed = [[] for _ in range(9)]
+    stacks = None
+    with open(input_file) as f:
+        for line in f.readlines():
+            if line.find('[')>=0:
+                for i in range(len(stacks_reversed)):
+                    if line[i*3+1+i] != ' ':
+                        stacks_reversed[i].append(line[i*3+1+i])
+                continue
+            
+            line = line.strip()
+            if line.startswith('#'):
+                break
+            if line == '':
+                stacks = reverse_stacks(stacks_reversed)
+            if line.startswith('move'):
+                line_a = line.split(' ')
+                move_crates(stacks,int(line_a[1]),int(line_a[3]),int(line_a[5]))
+            print(line)
+    for stack in stacks:
+        print(f'{stack.pop()}',end='')
+    print('')
+if __name__ == '__main__':
+    main()

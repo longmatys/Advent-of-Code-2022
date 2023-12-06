d22c3d673980979624b3d0c88f2644af12182167
Martin <longmatys@gmail.com>
2023-12-06 11:23:43 +0100

    Day 9 Part 2

--- day 09.py
+++ day 09.py
@@ -18,11 +18,12 @@ shorts={
 class pole:
     x_head: int = 0
     y_head: int = 0
-    tail_pole: list = field(default_factory=lambda: [[0,0] for _ in range(10)])
+    tail_pole: list = field(default_factory=lambda: [[0,0] for _ in range(9)])
     x_tail: int = 0
     y_tail: int = 0
     visited: set = field(default_factory=lambda: {tuple([0,0])})
     visited2: set = field(default_factory=lambda: {tuple([0,0])})
+    visited3: set = field(default_factory=lambda: {tuple([0,0])})
     
     def update_head(self,move):
         self.x_head+=move[0]
@@ -36,7 +37,19 @@ class pole:
         tail[0] += diff_x - shorts[diff_x]
         tail[1] += diff_y - shorts[diff_y]
         return tail
-    #def update_longtail(self):
+    def update_long_tail(self):
+        last_node = [self.x_head,self.y_head]
+        for node in self.tail_pole:
+            ret = self.get_new_position(last_node,node)
+            if ret:
+                last_node = node
+                
+            else:
+                break
+        #Real tail
+        self.visited2.add(tuple(self.tail_pole[-1]))
+        #Just a first knot
+        self.visited3.add(tuple(self.tail_pole[0]))
     def update_tail(self):
         ret = self.get_new_position([self.x_head,self.y_head],[self.x_tail,self.y_tail])
         if ret:
@@ -50,6 +63,7 @@ class pole:
             move = moves[direction]
             self.update_head(move)
             self.update_tail()
+            self.update_long_tail()
     
 def main():
 # Get the name of the Python script
@@ -67,7 +81,7 @@ def main():
             
             line_a = line.split(' ')
             moje_pole.move(line_a[0],int(line_a[1]))
-    print(moje_pole.visited)
-    print(len(moje_pole.visited), len(moje_pole.visited2))
+    #print(moje_pole.visited)
+    print(len(moje_pole.visited), len(moje_pole.visited2), len(moje_pole.visited3))
 if __name__ == '__main__':
     main()
